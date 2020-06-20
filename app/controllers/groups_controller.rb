bc80c8708cd64661b6ddc8071892b92430a0a949
bungotrick17 <bunrestarted@gmail.com>
2020-06-20 15:47:04 +0900

    users_edit

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,20 @@
+class GroupsController < ApplicationController
+  def new
+  end
+
+#   def create
+#     if current_group.update(group_params)
+#       redirect_to root_path
+#     else
+#       render :new
+#   end
+# end
+
+# private
+
+# def group_params
+#   params.require(:group).permit(:)
+
+# end
+
+end
